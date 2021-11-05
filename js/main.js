10658f6440dcfe3458a2049c80039fef1d327cee
Gonzalo Domingo Yagüez <gdomingoyaguez@alumnos.exa.unicen.edu.ar>
2021-11-05 19:55:44 -0300

    Agregamos un evento al boton que despliega un mensaje

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 alert ("Usar alert es mala práctica");
+
+let primerBtn = document.querySelector ("#primer-btn").addEventListener ("click", desplegarMensaje);
+
+function desplegarMensaje () {
+    cmensajeJS.innerHTML = "Desplegamos nuestro primer mensaje con un boton"
+}
