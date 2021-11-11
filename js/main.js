ac45aa08fceb0615fff62322570bfe3ad6f917aa
Gonzalo Salomon <salomongonzalo8@gmail.com>
2021-11-10 22:22:33 -0300

    Update main.js
    agrego una función para hacer visible/invisible un elemento con clase "hideable"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-alert ("Usar alert es mala práctica");
+alert("Usar alert es mala práctica");
 
-let primerBtn = document.querySelector ("#primer-btn").addEventListener ("click", desplegarMensaje);
-let mensajeJS = document.querySelector ("#mensajeJS")
+let primerBtn = document.querySelector("#primer-btn").addEventListener("click", desplegarMensaje);
+let segundoBtn = document.querySelector("#segundo-btn").addEventListener("click", () => {
+    alert("Aunque usar alert sea una mala práctica, explico lo que hace una arrow function:" +
+        "otra forma de usar funciones. Y oculto los elementos con la clase 'hideable'");
+    let hideableElems = document.querySelectorAll(".hideable");
+    for (let elem in hideableElems) {
+        if (elem.style.display != "none") {
+            elem.style.display = "none";
+        } else {
+            elem.style.display = "";
+        }
+    }
+});
+let mensajeJS = document.querySelector("#mensajeJS")
 
-function desplegarMensaje () {
+function desplegarMensaje() {
     mensajeJS.innerHTML = "Desplegamos nuestro primer mensaje con un boton"
 }
